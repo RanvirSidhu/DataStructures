111ae3c5a180491bc22965fb57b42b19f9425958
Ranvir Sidhu <ranbirsidhu55@gmail.com>
2021-02-17 12:49:11 +0530

    Updating

--- HashTable.cpp
+++ HashTable.cpp
@@ -17,9 +17,10 @@ void probe(int key)
     int tmph=hashfunc(key);
     for(int i=1;i<13;i++)
     {
-        if(h[tmph+i]==0)
+        int x=i;    //Linear Probing
+        if(h[tmph+x]==0)
         {
-            h[tmph+i]=key;
+            h[tmph+x]=key;
             break;
         }
     }
@@ -35,17 +36,62 @@ void insert(int key)
     }
     else
     {
-        probe(key);
-    }
-    
+        if(h[pos]!=key)
+        {
+            probe(key);
+        }
+    }    
 }
 
 void display()
 {
     for(int i=0;i<hsize;++i)
     {
-        cout<<h[i]<<" ";
+        if(h[i]==-1)
+        {
+            cout<<0<<" ";
+        }
+        else
+        {
+            cout<<h[i]<<" ";
+        }    
     }
+    cout<<endl;
+}
+
+int search(int num)
+{
+    int x=0;
+    int pos,tmp,flag=0;
+    pos=hashfunc(num);
+    tmp=pos;
+    while(h[tmp]!=0)
+    {
+        if(h[tmp]==num)
+        {
+            flag=1;
+            break;
+        }
+        else
+        {
+            x++;
+            tmp=pos+x; 
+        }
+    }
+    if(flag==0)
+    {
+        cout<<endl<<"The number does not exist";
+        exit(0);
+    }
+    return tmp;
+}
+    
+
+void deletion(int num)
+{
+    int pos;
+    pos=search(num);
+    h[pos]=-1;  //tombstone = -1
 }
 
 int main()
@@ -56,7 +102,10 @@ int main()
     insert(9);
     insert(20);
     insert(1);
+    insert(14);
     insert(13);
+    display();
+    deletion(15);
     display();
     return 0;
 }
