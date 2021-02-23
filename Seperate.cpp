0246282c8dcf7998fe7c2ceee3787ed9d1cbc332
Ranvir Sidhu <ranbirsidhu55@gmail.com>
2021-02-23 19:27:16 +0530

    Seperate

--- Seperate.cpp
+++ Seperate.cpp
@@ -33,29 +33,102 @@ void display()
 {
     list <int> :: iterator it;
     cout<<"The Hashmap is: ";
+    cout<<endl;
     for(int i=0;i<hsize;++i)
     {
+        cout<<"["<<i<<"]: ";
         if(Table[i]==NULL)
         {
+            cout<<endl;
             continue;
         }
         else
         {
             it=Table[i]->begin();
-            cout<<endl;
             for(it;it!=Table[i]->end();++it)
             {
                 cout<<*it<<" ";
             }
         }
+        cout<<endl;
+    }
+}
+
+int search(int num,bool print)
+{
+    list <int> :: iterator it;
+    int hkey,pos=0;
+    bool flag=false;
+    hkey=hashfunc(num);
+    if(Table[hkey]==NULL)
+    {
+        cout<<"The number doesnt exist";
+    }
+    else
+    {
+        for(it=Table[hkey]->begin();it!=Table[hkey]->end();++it)
+        {
+            if(*it==num)
+            {
+                flag=true;
+                break;
+            }
+            pos++; 
+        }
+    }
+    if(print==true)
+    {
+        if(flag==false)
+        {
+            cout<<"The number doesn't exist";
+        }
+        else
+        {
+            cout<<"The number does exist";
+            cout<<pos;
+        }
+    }
+    else
+    {
+        if(flag==false)
+        {
+            pos=-1;
+        }
+    }
+    return pos;
+}
+
+void deletion(int num)
+{
+    list <int> :: iterator it;
+    int pos,hkey;
+    hkey=hashfunc(num);
+    pos=search(num,false);
+    it=Table[hkey]->begin();
+    if(pos==-1)
+    {
+        cout<<"The number doesnt exist"<<endl;
+    }
+    else
+    {
+        advance(it,pos);
+        Table[hkey]->erase(it);
+        display();
     }
 }
 
 int main()
 {
+    insert(5);
     insert(1);
     insert(2);
     insert(6);
+    insert(13);
+    insert(19);
+    insert(24);
+    insert(39);
     display();
+    deletion(25);
+    //search(3,false);
     return 0;
 }
